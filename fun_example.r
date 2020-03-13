248bc1fa9c791ad29291e8e92573fa6f30b0bb62
dlgeraghty <dvd.loge@gmail.com>
2020-03-13 14:01:03 +0100

    printing a variable in R

--- fun_example.r
+++ fun_example.r
@@ -2,4 +2,6 @@
 
 #assignment: can be achieved with = or <-
 #example: (variable names can have . )
-header.beaver1 = head(beaver1)
+header.beaver1 <- head(beaver1)
+#printing a variable:
+print(header.beaver1)
