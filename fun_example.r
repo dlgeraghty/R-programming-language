8b41232b07384b770efd58eeebaa53a1fa12c733
dlgeraghty <dvd.loge@gmail.com>
2020-03-13 18:22:52 +0100

    plottinggit add Rplots.pdf

--- fun_example.r
+++ fun_example.r
@@ -241,13 +241,33 @@
 #}
 #divide(10, "5")
 
-#file i/o
-myPeople = read.table(file="people.txt",
-			header=T, sep=" ",
-			na.strings=" ",
-			stringsAsFactors=F)
-myPeople
+##file i/o
+#myPeople = read.table(file="people.txt",
+#			header=T, sep=" ",
+#			na.strings=" ",
+#			stringsAsFactors=F)
+#myPeople
 
+#plotting:
+xy1 = matrix(data=c(1,2,3,4,5,1,2,3,4,5), nrow=5, ncol=5)
+plot(xy1)	#when ploting, if you are using something as Rscript as I do, your plot will be generated as a pdf file :)
+
+x2 = c(1,2,3,4,5)
+y2 = c(1,2,3,4,5)
+plot(x2, y2, type = "l")	#actually all the plots will be outputed to the same file , thats nice !
+plot(x2, y2, type = "b")
+plot(x2, y2, type = "o", main = "My Plot" , xlab = "x axis", ylab = "y axis", col = "steelblue")
+plot(x2, y2, type = "o", pch = 2, lty = 2, main = "My Plot" , xlab = "x axis", ylab = "y axis", col = "steelblue", xlim = c(-8, 8), ylim = c(-8,8))
+
+plot(x2, y2, type = "b")
+abline(h = c(2,4), col = "red" , lty = 2)
+segments(x0=c(2,4), y0= c(2,2), x1 = c(2,4), y1 = c(4,4), col = "red" , lty = 2)
+arrows(x0 = 1.5, y0 = 4.5, x1 = 2.7, y1 = 3.25, col = "blue")
+text(x = 1.25, y = 4.75, labels = "Center")
+
+plot(faithful)
+eruptions4 = with(faithful, faithful[eruptions > 4, ])
+points(eruptions4, col = "red", pch = 3)
 
 
 
