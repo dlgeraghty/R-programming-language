919b2184eb440b8d2fa623b05f9fd7ebe22a29f9
dlgeraghty <dvd.loge@gmail.com>
2020-03-13 14:04:06 +0100

    data types in R

--- fun_example.r
+++ fun_example.r
@@ -4,4 +4,12 @@
 #example: (variable names can have . )
 header.beaver1 <- head(beaver1)
 #printing a variable:
-print(header.beaver1)
+#print(header.beaver1)
+
+#types of data in R:
+print(class(4))
+print(class(4L))
+print(class(T))
+print(class(1 + 4i))
+print(class("Sample"))
+print(class(charToRaw("Sample")))
