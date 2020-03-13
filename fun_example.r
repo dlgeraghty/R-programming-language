2206ebe8e916fcefb750c8113c21b207ed563c5b
dlgeraghty <dvd.loge@gmail.com>
2020-03-13 14:13:27 +0100

    arithmetic operators

--- fun_example.r
+++ fun_example.r
@@ -2,14 +2,22 @@
 
 #assignment: can be achieved with = or <-
 #example: (variable names can have . )
-header.beaver1 <- head(beaver1)
+#header.beaver1 <- head(beaver1)
 #printing a variable:
 #print(header.beaver1)
 
 #types of data in R:
-print(class(4))
-print(class(4L))
-print(class(T))
-print(class(1 + 4i))
-print(class("Sample"))
-print(class(charToRaw("Sample")))
+#print(class(4))
+#print(class(4L))
+#print(class(T))
+#print(class(1 + 4i))
+#print(class("Sample"))
+#print(class(charToRaw("Sample")))
+
+#Arithmetic operators:
+sprintf("4 + 5 = %d" , 4 + 5)
+sprintf("4 - 5 = %d" , 4 - 5)
+sprintf("4 * 5 = %d" , 4 * 5)
+sprintf("4 / 5 = %1.3f" , 4 / 5)
+sprintf("4 %% 5 = %d" , 4 %% 5)
+sprintf("4 ^ 5 = %d" , 4^5)
