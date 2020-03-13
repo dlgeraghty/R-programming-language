00cb758e7e6229be7abb07c18ba7743e148d8782
dlgeraghty <dvd.loge@gmail.com>
2020-03-13 21:02:38 +0100

    display cars with auto and manual transmission

--- cars.r
+++ cars.r
@@ -1,6 +1,10 @@
 data(mtcars)
 mtcars
 #rownames(mtcars) 	#to extract the lables from the dataset
+#correctly plotting the mtcars$mpg entry in a barplot, with its labels
 op <- par(mar=c(4,10,2,2))
 barplot(mtcars$mpg, main = "the mtcars dataset", names.arg = rownames(mtcars), horiz=T, las=2, col = "skyblue")
 rm(op)
+#display the cars that have automatic and manual transmission
+count = table(mtcars$am)
+barplot(count, main = "transmission", names.arg=c("automatic", "manual"), col = "red")
